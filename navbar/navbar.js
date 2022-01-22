ac5ff319b910793f75b59b1deacb39afe7292583
SujoyGhosh-scott <ghoshsujoy336@gmail.com>
2022-01-23 00:33:28 +0530

    about, contact, navbar added

---
+++ navbar/navbar.js
@@ -0,0 +1,9 @@
+function openNav() {
+  console.log("opening");
+  document.getElementById("mySidebar").style.right = "0";
+}
+
+function closeNav() {
+  console.log("closing");
+  document.getElementById("mySidebar").style.right = "-200px";
+}
