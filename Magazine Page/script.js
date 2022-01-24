2947310ec30b7ee8d71efff59ac98fc1837487d6
Chayan Kumar Sengupta <swapnomoy199916@gmail.com>
2022-01-24 18:25:22 +0530

    3d effect added

--- Magazine Page/script.js
+++ Magazine Page/script.js
@@ -1,6 +1,15 @@
 const swiper = new Swiper(".swiper", {
   // Optional parameters
-  direction: "horizontal",
+
+  effect: "cube",
+  grabCursor: true,
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
+
   // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
