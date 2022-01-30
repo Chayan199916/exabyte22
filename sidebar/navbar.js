fb3486479df30447bf5f51474cdeb6f7a962c1b8
SujoyGhosh-scott <ghoshsujoy336@gmail.com>
2022-01-30 20:25:01 +0530

    contact page update

--- sidebar/navbar.js
+++ sidebar/navbar.js
@@ -1,11 +1,11 @@
 function openNav() {
-  console.log("opening");
+  //console.log("opening");
   document.getElementById("mySidebar").style.right = "0";
   document.getElementById("open").style.display = "none";
 }
 
 function closeNav() {
-  console.log("closing");
+  //console.log("closing");
   document.getElementById("mySidebar").style.right = "-190px";
   document.getElementById("open").style.display = "block";
 }
