a359b6f94d43a00c430290b8f5c500f7e5fa0da4
Suvranil Dutta Biswas <suvranilduttabiswas@gmail.com>
2022-02-04 11:41:01 +0530

    timer fix

--- screens/AboutScreen/script.js
+++ screens/AboutScreen/script.js
@@ -7,24 +7,33 @@ function countDown() {
     let currentTime = now.getTime();
     let remainingTime = eventTime - currentTime;
 
-    let seconds = Math.floor(remainingTime / 1000);
-    let minutes = Math.floor(seconds / 60);
-    let hours = Math.floor(minutes / 60);
-    let days = Math.floor(hours / 24);
+    if (remainingTime < 0) {
+        document.getElementById('days').textContent = "00";
+        document.getElementById('hours').textContent = "00";
+        document.getElementById('minutes').textContent = "00";
+        document.getElementById('seconds').textContent = "00";
+    } else {
 
-    hours %= 24;
-    minutes %= 60;
-    seconds %= 60;
+        let seconds = Math.floor(remainingTime / 1000);
+        let minutes = Math.floor(seconds / 60);
+        let hours = Math.floor(minutes / 60);
+        let days = Math.floor(hours / 24);
+    
+        hours %= 24;
+        minutes %= 60;
+        seconds %= 60;
+    
+        days = (days < 10) ? "0" + days : days;
+        hours = (hours < 10) ? "0" + hours : hours;
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+    
+        document.getElementById('days').textContent = days;
+        document.getElementById('hours').textContent = hours;
+        document.getElementById('minutes').textContent = minutes;
+        document.getElementById('seconds').textContent = seconds;
+    }
 
-    days = (days < 10) ? "0" + days : days;
-    hours = (hours < 10) ? "0" + hours : hours;
-    minutes = (minutes < 10) ? "0" + minutes : minutes;
-    seconds = (seconds < 10) ? "0" + seconds : seconds;
-
-    document.getElementById('days').textContent = days;
-    document.getElementById('hours').textContent = hours;
-    document.getElementById('minutes').textContent = minutes;
-    document.getElementById('seconds').textContent = seconds;
 
 }
 
